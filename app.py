df1c48f995efe0c9a121deb119a801be40fd16f2
pl366 <pl366@snu.edu.in>
2018-08-25 20:43:36 +0530

    ibm cloud testing4

--- app.py
+++ app.py
@@ -4,18 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World"
+	return "Hello World"
 
 
 @app.route('/ibm')
 def ibm_score():
 		tone_analyzer = ToneAnalyzerV3(
 		version='2017-09-21',
-	    username="27edc35f-d026-4dd3-930e-bac1f6fe10ef",
-	    password="FmEiqWUBJmDN"
-	    )
+		username="27edc35f-d026-4dd3-930e-bac1f6fe10ef",
+		password="FmEiqWUBJmDN")
 	return "IBM WATSON API RETURN REVIEW SENTIMENT"
 
 
 if __name__ == "__main__":
-   app.run()
+	app.run()
