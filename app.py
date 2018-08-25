30a55dbc7719754775f828c39bbc8c9ebbc20aab
pl366 <pl366@snu.edu.in>
2018-08-25 20:53:04 +0530

    ibm cloud testing6

--- app.py
+++ app.py
@@ -13,7 +13,26 @@ def ibm_score():
 	version='2017-09-21',
 	username="27edc35f-d026-4dd3-930e-bac1f6fe10ef",
 	password="FmEiqWUBJmDN")
-	s= "saatvik"
+
+	text = "Fuck your Product"
+
+	tone_analysis = tone_analyzer.tone({'text': text},'application/json')
+	j = json.dumps(tone_analysis, indent=2)
+	d = json.loads(j)
+	print (d)
+
+	intent = {"Anger":2,"Joy":5,"Sadness":3,"Fear":4,"Confident":3,"Tentative":1,"Analytical":2}
+
+	i = 0
+	s = 0
+	while True:
+		try:
+			s+=(intent[d["document_tone"]["tones"][i]["tone_name"]])*(d["document_tone"]["tones"][i]["score"])
+			i+=1
+		except:
+			break	
+
+	print (s)	
 	return s 
 
 if __name__ == "__main__":
