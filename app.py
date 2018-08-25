395211da0847aa3f91343d2c0ec2234be81775dd
pl366 <pl366@snu.edu.in>
2018-08-25 20:48:48 +0530

    ibm cloud testing5

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ def hello_world():
 
 @app.route('/ibm')
 def ibm_score():
-		tone_analyzer = ToneAnalyzerV3(
-		version='2017-09-21',
-		username="27edc35f-d026-4dd3-930e-bac1f6fe10ef",
-		password="FmEiqWUBJmDN")
-	return "IBM WATSON API RETURN REVIEW SENTIMENT"
-
+	tone_analyzer = ToneAnalyzerV3(
+	version='2017-09-21',
+	username="27edc35f-d026-4dd3-930e-bac1f6fe10ef",
+	password="FmEiqWUBJmDN")
+	s= "saatvik"
+	return s 
 
 if __name__ == "__main__":
 	app.run()
+ 
