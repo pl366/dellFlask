9ae5f9b357d979caee43a9d97ea442c9f1b6b9ad
pl366 <pl366@snu.edu.in>
2018-08-25 20:12:26 +0530

    ibm cloud requirement added

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from watson_developer_cloud import ToneAnalyzerV3
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
    return "Hello World"
+
+
+@app.route('/ibm')
+def ibm_score():
+		tone_analyzer = ToneAnalyzerV3(
+		version='2017-09-21',
+	    username="27edc35f-d026-4dd3-930e-bac1f6fe10ef",
+	    password="FmEiqWUBJmDN"
+	    )
+
 
 if __name__ == "__main__":
    app.run()
